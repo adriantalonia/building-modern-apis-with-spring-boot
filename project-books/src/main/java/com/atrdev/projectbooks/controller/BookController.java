855cad734fceb0263ee7e3c9c6fd9e7a5afac10d
Adrian Talonia <adrian.talonia11@gmail.com>
2025-05-22 19:33:03 -0600

    add new changes about project 2 related to validation pathvariable

--- project-books/src/main/java/com/atrdev/projectbooks/controller/BookController.java
+++ project-books/src/main/java/com/atrdev/projectbooks/controller/BookController.java
@@ -1,6 +1,8 @@
 package com.atrdev.projectbooks.controller;
 
-import com.atrdev.projectbooks.entity.Book;
+import com.atrdev.projectbooks.model.dto.BookRequest;
+import com.atrdev.projectbooks.model.entity.Book;
+import jakarta.validation.constraints.Min;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,10 +28,75 @@ public class BookController {
 
     private void initializeBooks() {
         books.addAll(List.of(
-                new Book("Title one", "Author one", "Science"),
-                new Book("TItle two", "Author two", "Math")
+                new Book(1, "Computer Science Pro", "Chad Darby", "Computer Science", 5),
+                new Book(2, "Java Spring Master", "Eric Roby", "Computer Science", 5),
+                new Book(3, "Why 1+1 Rocks", "Adil A.", "Math", 5),
+                new Book(4, "How Bears Hibernate", "Bob B.", "Science", 2),
+                new Book(5, "A Pirate's Treasure", "Curt C.", "History", 3),
+                new Book(6, "Why 2+2 is Better", "Dan D.", "Math", 1)
         ));
     }
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable @Min(value = 1) long id) {
+        return books.stream()
+                .filter(book -> book.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+
+    @PostMapping
+    public boolean addBook(@RequestBody BookRequest bookRequest) {
+        long id = books.isEmpty() ? 1 : books.getLast().getId() + 1;
+        Book book = convertToBook(bookRequest, id);
+        return books.add(book);
+    }
+
+    @PutMapping("/{id}")
+    public boolean updateBook(@PathVariable @Min(value = 1) long id, @RequestBody BookRequest bookRequest) {
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).getId() == id) {
+                Book book = convertToBook(bookRequest, id);
+                books.set(i, book);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteBook(@PathVariable @Min(value = 1) long id) {
+        return books.removeIf(book -> book.getId() == id);
+    }
+
+    private Book convertToBook(BookRequest bookRequest, long id) {
+        return new Book(id, bookRequest.getTitle(), bookRequest.getAuthor(), bookRequest.getCategory(), bookRequest.getRating());
+    }
+
+    /*@PutMapping("/{id}")
+    public boolean updateBook(@PathVariable long id, @RequestBody Book book) {
+        Book existingBook = books.stream()
+                .filter(b -> b.getId() == id)
+                .findFirst()
+                .orElse(null);
+        if (existingBook != null) {
+            existingBook.setAuthor(book.getAuthor());
+            existingBook.setCategory(book.getCategory());
+            existingBook.setTitle(book.getTitle());
+            return true;
+        }
+        return false;
+    }*/
+
+    /*@PostMapping
+    public boolean addBook(@RequestBody Book book) {
+        boolean isNewBook = books.stream()
+                .noneMatch(b -> b.getTitle().equalsIgnoreCase(book.getTitle()));
+        if (isNewBook) {
+            books.add(book);
+        }
+        return isNewBook;
+    }*/
 
     /*@GetMapping
     public List<Book> getBooks() {
@@ -46,13 +113,30 @@ public class BookController {
         return null;
     }*/
 
-    @GetMapping("/{title}")
+    /*@GetMapping("/{title}")
     public Book getBookByTitle(@PathVariable String title) {
         return books.stream()
                 .filter(book -> book.getTitle().equalsIgnoreCase(title))
                 .findFirst()
                 .orElse(null);
-    }
+    }*/
+
+    /*@PutMapping("/{title}")
+    public boolean updateBook(@PathVariable String title, @RequestBody Book book) {
+        Book existingBook = books.stream()
+                .filter(b -> b.getTitle().equalsIgnoreCase(title))
+                .findFirst()
+                .orElse(null);
+        if (existingBook != null) {
+            existingBook.setAuthor(book.getAuthor());
+            existingBook.setCategory(book.getCategory());
+            existingBook.setTitle(book.getTitle());
+            return true;
+        }
+        return false;
+    }*/
+
+
 
     @GetMapping
     public List<Book> getBooksByCategory(@RequestParam(required = false) String category) {
@@ -82,35 +166,4 @@ public class BookController {
         books.add(book);
         return true;
     }*/
-
-    @PostMapping
-    public boolean addBook(@RequestBody Book book) {
-        boolean isNewBook = books.stream()
-                .noneMatch(b -> b.getTitle().equalsIgnoreCase(book.getTitle()));
-        if (isNewBook) {
-            books.add(book);
-        }
-        return isNewBook;
-    }
-
-    @PutMapping("/{title}")
-    public boolean updateBook(@PathVariable String title, @RequestBody Book book) {
-        Book existingBook = books.stream()
-                .filter(b -> b.getTitle().equalsIgnoreCase(title))
-                .findFirst()
-                .orElse(null);
-        if (existingBook != null) {
-            existingBook.setAuthor(book.getAuthor());
-            existingBook.setCategory(book.getCategory());
-            existingBook.setTitle(book.getTitle());
-            return true;
-        }
-        return false;
-    }
-
-    @DeleteMapping("/{title}")
-    public boolean deleteBook(@PathVariable String title) {
-        return books.removeIf(book -> book.getTitle().equalsIgnoreCase(title));
-    }
-
 }
--- project-books/src/main/java/com/atrdev/projectbooks/model/dto/BookRequest.java
+++ project-books/src/main/java/com/atrdev/projectbooks/model/dto/BookRequest.java
@@ -1,40 +1,41 @@
-package com.atrdev.projectbooks.entity;
+package com.atrdev.projectbooks.model.dto;
 
-public class Book {
-
+public class BookRequest {
     private String title;
     private String author;
     private String category;
+    private int rating;
 
-    public Book() {}
-
-    public Book(String title, String author, String category) {
+    public BookRequest() {
+    }
+    public BookRequest(String title, String author, String category, int rating) {
         this.title = title;
         this.author = author;
         this.category = category;
+        this.rating = rating;
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public String getAuthor() {
         return author;
     }
-
     public void setAuthor(String author) {
         this.author = author;
     }
-
     public String getCategory() {
         return category;
     }
-
     public void setCategory(String category) {
         this.category = category;
+    }
+    public int getRating() {
+        return rating;
+    }
+    public void setRating(int rating) {
+        this.rating = rating;
     }
 }
