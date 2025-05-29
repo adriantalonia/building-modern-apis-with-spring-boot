bf0e25e01047d27eabfb8009098c0b45631ec45d
Adrian Talonia <adrian.talonia11@gmail.com>
2025-05-28 22:03:35 -0600

    add project employee

---
+++ project-employees/src/test/java/com/atrdev/projectemployees/ProjectEmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atrdev.projectemployees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectEmployeesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
