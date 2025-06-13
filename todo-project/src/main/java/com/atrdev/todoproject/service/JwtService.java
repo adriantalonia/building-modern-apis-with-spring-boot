8e0900de5145de85b8d6450df6d392a0fef081b0
Adrian Talonia <adrian.talonia11@gmail.com>
2025-06-12 22:33:04 -0600

    add jwt configuration and filter

---
+++ todo-project/src/main/java/com/atrdev/todoproject/service/JwtService.java
@@ -0,0 +1,11 @@
+package com.atrdev.todoproject.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Map;
+
+public interface JwtService {
+    String extractUsername(String token);
+    Boolean isTokenValid(String token, UserDetails userDetails);
+    String generateToken(Map<String, Object> claims, UserDetails userDetails);
+}
---
+++ todo-project/src/main/java/com/atrdev/todoproject/service/JwtServiceImpl.java
@@ -0,0 +1,69 @@
+package com.atrdev.todoproject.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.Map;
+
+@Service
+public class JwtServiceImpl implements JwtService {
+
+    private final String SECRET_KEY;
+    private final long JWT_EXPIRATION;
+
+    public JwtServiceImpl(@Value("${spring.jwt.secret}") String secretKey,
+                          @Value("${spring.jwt.expiration}") long jwtExpiration) {
+        SECRET_KEY = secretKey;
+        JWT_EXPIRATION = jwtExpiration;
+    }
+
+    @Override
+    public String generateToken(Map<String, Object> claims, UserDetails userDetails) {
+        return Jwts.builder()
+                .claims(claims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION))
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    @Override
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    @Override
+    public Boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private SecretKey getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractAllClaims(token).getExpiration();
+    }
+}
